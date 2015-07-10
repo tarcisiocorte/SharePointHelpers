5cadb6744bf3befc5aeb62825eba06651aa285cd
unknown <tcosta@pobdubltcosta.adm.ie>
2015-07-10 09:41:43 +0100

    Initalizing project

---
+++ TC.SharePoint.Utilities/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SharePoint;
+
+namespace TC.SharePoint.Utilities
+{
+    public static partial class Utils
+    {
+        /// <summary>
+        /// Remove anonymous Access on List
+        /// </summary>
+        /// <param name="web"></param>
+        /// <param name="listTitle"></param>
+        /// <returns></returns>
+        public static bool RemoveAnonymousAccess(SPWeb web, string listTitle)
+        {
+            SPList list = web.Lists.TryGetList(listTitle);
+            if (list != null && list.HasUniqueRoleAssignments)
+            {
+                list.AnonymousPermMask64 = SPBasePermissions.EmptyMask;
+                list.Update();
+                return true;
+            }
+            return false;
+        }
+    }
+}
