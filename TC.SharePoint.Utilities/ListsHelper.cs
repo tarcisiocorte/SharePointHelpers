d4389f3ad3464ce478e025c2b18882f439b89a54
unknown <tcosta@pobdubltcosta.adm.ie>
2015-07-27 17:37:35 +0100

    Add methods to create library

--- TC.SharePoint.Utilities/ListsHelper.cs
+++ TC.SharePoint.Utilities/ListsHelper.cs
@@ -9,6 +9,39 @@ namespace TC.SharePoint.Utilities
     public static class ListsHelper
     {
 
+        public static SPList EnsureFormLibraryCreation(SPWeb web, string listName, string listTitle, string listDescription)
+        {
+            //return EnsureListCreation(web, listName, listTitle, listDescription, SPListTemplateType.DocumentLibrary);
+            return EnsureListCreation(web, listName, listTitle, listDescription, SPListTemplateType.XMLForm);
+            //return EnsureListCreation(web, listName, listTitle, listDescription, SPListTemplateType.GenericList);
+        }
+
+        /// <summary>
+        /// Create a library as Data Connection Library
+        /// </summary>
+        /// <param name="web"></param>
+        /// <param name="listName"></param>
+        /// <param name="listTitle"></param>
+        /// <param name="listDescription"></param>
+        /// <returns></returns>
+        public static SPList EnsureDataConnectionLibraryCreation(SPWeb web, string listName, string listTitle, string listDescription)
+        {   
+            return EnsureListCreation(web, listName, listTitle, listDescription, SPListTemplateType.DataConnectionLibrary);
+        }
+
+        /// <summary>
+        /// Create a library as a Document Library
+        /// </summary>
+        /// <param name="web"></param>
+        /// <param name="listName"></param>
+        /// <param name="listTitle"></param>
+        /// <param name="listDescription"></param>
+        /// <returns></returns>
+        public static SPList EnsureDocumentLibraryCreation(SPWeb web, string listName, string listTitle, string listDescription)
+        {            
+            return EnsureListCreation(web, listName, listTitle, listDescription, SPListTemplateType.DocumentLibrary);
+        }
+
         /// <summary>
         /// Create a new list
         /// </summary>
