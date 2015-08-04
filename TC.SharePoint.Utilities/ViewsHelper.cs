af884ba593b957b20524ded45a274dd87ccd4998
unknown <tcosta@pobdubltcosta.adm.ie>
2015-08-04 15:50:32 +0100

    Add metho AddViewToListShowOnlyFiles

--- TC.SharePoint.Utilities/ViewsHelper.cs
+++ TC.SharePoint.Utilities/ViewsHelper.cs
@@ -27,6 +27,24 @@ namespace TC.SharePoint.Utilities
             view.Update();
         }
 
+        /// <summary>
+        /// This method create view in order to show files of a list without show the folders of files.
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="viewTitle"></param>
+        /// <param name="fieldsToShow"></param>
+        public static void AddViewToListShowOnlyFiles(SPList list, string viewTitle, string[] fieldsToShow)
+        {
+            string viewName = GetValidStrForUrl(viewTitle);
+            System.Collections.Specialized.StringCollection collViewFields = new System.Collections.Specialized.StringCollection();
+            foreach (string fieldStr in fieldsToShow)
+                collViewFields.Add(list.Fields.GetField(fieldStr).InternalName);
+            SPView view = list.Views.Add(viewName, collViewFields, null, 30, true, false);
+            view.Scope = SPViewScope.Recursive;
+            view.Title = viewTitle;
+            view.Update();
+        }
+
         private static string GetValidStrForUrl(string viewTitle)
         {
             return viewTitle.Replace(" ", "");
